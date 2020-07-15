2f221d57d882eb13616cd01596ffd6992fa34f14
SparklingIsGood <marc.desperiez@orange.fr>
2020-07-15 14:53:49 +0200

    Changed the way StatusLabel works, fixed a generation issue.

--- Minecraft Sparkling Server Hosting Tool/ServerCompatibleForm.cs
+++ Minecraft Sparkling Server Hosting Tool/ServerCompatibleForm.cs
@@ -27,7 +27,7 @@ namespace Minecraft_Sparkling_Server_Hosting_Tool
             this.Close();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e)
         {
             if (Client.Text == "Vanilla (Normal Minecraft)")
             {
@@ -45,7 +45,45 @@ namespace Minecraft_Sparkling_Server_Hosting_Tool
                 return;
             }
             StatusLabel.Text = "Generating Vanilla.txt file... \n\n Generating tempserver.properties file...";
-            File.Create(main.ServerDirectory + @"\tempserver.properties");
+            using (StreamWriter sw = File.CreateText(main.ServerDirectory + @"\" + "tempserver.properties"))
+            {
+                await sw.WriteLineAsync("#Minecraft server properties");
+                await sw.WriteLineAsync("#Fri Jul 01 00:00:00 CEST 2020");
+                await sw.WriteLineAsync("generator-settings=");
+                await sw.WriteLineAsync("op-permission-level=4");
+                await sw.WriteLineAsync("allow-nether=true");
+                await sw.WriteLineAsync("level-name=world");
+                await sw.WriteLineAsync("enable-query=false");
+                await sw.WriteLineAsync("allow-flight=false");
+                await sw.WriteLineAsync("announce-player-achievements=true");
+                await sw.WriteLineAsync("server-port=25565");
+                await sw.WriteLineAsync("max-world-size=29999984");
+                await sw.WriteLineAsync("level-type=DEFAULT");
+                await sw.WriteLineAsync("enable-rcon=false");
+                await sw.WriteLineAsync("level-seed=");
+                await sw.WriteLineAsync("force-gamemode=false");
+                await sw.WriteLineAsync("server-ip=");
+                await sw.WriteLineAsync("network-compression-threshold=256");
+                await sw.WriteLineAsync("max-build-height=256");
+                await sw.WriteLineAsync("spawn-npcs=true");
+                await sw.WriteLineAsync("white-list=false");
+                await sw.WriteLineAsync("spawn-animals=true");
+                await sw.WriteLineAsync("hardcore=false");
+                await sw.WriteLineAsync("snooper-enabled=true");
+                await sw.WriteLineAsync("resource-pack-sha1=");
+                await sw.WriteLineAsync("online-mode=true");
+                await sw.WriteLineAsync("resource-pack=");
+                await sw.WriteLineAsync("pvp=true");
+                await sw.WriteLineAsync("difficulty=1");
+                await sw.WriteLineAsync("enable-command-block=true");
+                await sw.WriteLineAsync("gamemode=0");
+                await sw.WriteLineAsync("player-idle-timeout=0");
+                await sw.WriteLineAsync("max-players=20");
+                await sw.WriteLineAsync("spawn-monsters=true");
+                await sw.WriteLineAsync("generate-structures=true");
+                await sw.WriteLineAsync("view-distance=10");
+                await sw.WriteLineAsync("motd=A Minecraft Server");
+            }
             StatusLabel.Text = "Generating Vanilla.txt file... \n\n Generating tempserver.properties file... \n\n Done!";
             MessageBox.Show("Your server is now compatible with our tool!", "Done", MessageBoxButtons.OK, MessageBoxIcon.None);
             this.Close();
